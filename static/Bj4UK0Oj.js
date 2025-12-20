58beae2686a9bfac8ac1336d8e52ddcc483bf1f1
gaea3 <gaea3@qq.com>
2025-12-20 13:43:56 +0800

    update statis

--- static/Bj4UK0Oj.js
+++
@@ -1,1 +0,0 @@
-import{T as e,o as r}from"./C7wrhjpR.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};
---
+++ static/tuMmjJ0U.js
@@ -0,0 +1,1 @@
+import{T as e,o as r}from"./CBxaS7zh.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};

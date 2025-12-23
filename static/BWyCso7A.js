0a9e009b78c2b6df6d8866dfb489d1d328b4c102
gaea3 <develop@aigaea.net>
2025-12-23 11:07:29 +0000

    update ui

---
+++ static/BWyCso7A.js
@@ -0,0 +1,1 @@
+import{T as e,o as r}from"./CzK5vXRu.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};
--- static/router-login.2bb589be.js
+++
@@ -1,1 +0,0 @@
-import{b_ as o,X as e}from"./entry.819de5b2.js";import{u as t}from"./user.fa958ce8.js";const a=o((r,s)=>{if(t().getToken){if(r.path==="/")return e("/home")}else return e("/login")});export{a as default};

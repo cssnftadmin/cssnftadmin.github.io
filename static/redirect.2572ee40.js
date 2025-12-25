9f3fc6618bb5d58e9a7814a3292d5b299d43f919
gaea3 <develop@aigaea.net>
2025-12-25 09:05:27 +0000

    update 2025-12-25 09:05:27

---
+++ static/redirect.2572ee40.js
@@ -0,0 +1,1 @@
+import{c as e,a as r}from"./entry.c4649681.js";const _={__name:"redirect",setup(t){return(c,a)=>(r(),e("div"))}};export{_ as default};
---
+++ static/router-login.249de3be.js
@@ -0,0 +1,1 @@
+import{bQ as a,u as n,n as r}from"./entry.c4649681.js";const u=a((e,s)=>{const t=n(),o=document.querySelector(".body-container");if(o&&o.scrollTo({top:0}),e.path==="/dashboard"||e.path==="/dashboard/")return r("/");if(!t.getCssNftToken)return r("/login")});export{u as default};
